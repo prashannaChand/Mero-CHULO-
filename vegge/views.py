abcb1c62697f1d878bf638574e5a31bbd966e71f
PrashannaChand <chandprashanna99@gmail.com>
2025-05-20 14:52:07 +0545

    first datat

--- vegge/views.py
+++ vegge/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def recipes(request):
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+        print(data)
+        print(image)
+
+        Recipe.objects.create(
+            name=data.get('name'),
+            description=data.get('description'),
+            image=image
+        )
+        # You can add logic to save the data here
     return render(request, 'recipes.html')
