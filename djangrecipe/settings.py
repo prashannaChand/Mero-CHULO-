9041528a1717923c3bd9820d220d6a89aa651e69
PrashannaChand <chandprashanna99@gmail.com>
2025-06-04 12:18:58 +0545

    render setup

--- djangrecipe/settings.py
+++ djangrecipe/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-x_ob$qfm(tlg9b67ug8=@*9fq&xcl4$g#ul1t4k2h35ra1b56$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mero-chulo-1.onrender.com', 'localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://mero-chulo-1.onrender.com',
+]
+CORS_ALLOWED_ORIGINS = [
+    'https://mero-chulo-1.onrender.com',
+]
 
 
 # Application definition
